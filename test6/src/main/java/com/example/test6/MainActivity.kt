670f313e74da932a6e07948cd01fc88958e4bdf7
hanhojjung <ghwjd9269@naver.com>
2023-05-11 11:25:08 +0900

    2023 05 11 3교시 - 1

--- test6/src/main/java/com/example/test6/MainActivity.kt
+++ test6/src/main/java/com/example/test6/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.test6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html.ImageGetter
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() { //AppCompoatActivity 부모 클래스로부터 상속 받음
     // 코드 작업 공간, 뷰 작업도 가능하지만 가급적 뷰는 xml에서 작업하기
@@ -15,5 +19,20 @@ class MainActivity : AppCompatActivity() { //AppCompoatActivity 부모 클래스
         // 화면을 출력하겠다. 레이아웃 7장 더 설명.
         // 뷰그룹 : 뷰(텍스트, 이미지, 인풋, 체크박스, 라디오..)를 모아주는 역할
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.btn1)
+        val img1 = findViewById<ImageView>(R.id.img1)
+        var status = 0
+
+        //button1 눌러서 -> img show/hide
+        button1.setOnClickListener {
+            if (status == 0) {
+                img1.visibility = View.INVISIBLE
+                status = 1
+            } else {
+                img1.visibility = View.VISIBLE
+                status = 0
+            }
+        }
     }
 }

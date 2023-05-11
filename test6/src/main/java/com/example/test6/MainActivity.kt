092f7355c0983b1482b93d50f297d24a9e608e12
hanhojjung <ghwjd9269@naver.com>
2023-05-11 12:36:32 +0900

    2023 05 11 4교시 - 2

--- test6/src/main/java/com/example/test6/MainActivity.kt
+++ test6/src/main/java/com/example/test6/MainActivity.kt
@@ -6,6 +6,7 @@ import android.text.Html.ImageGetter
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import com.example.test6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() { //AppCompoatActivity 부모 클래스로부터 상속 받음
     // 코드 작업 공간, 뷰 작업도 가능하지만 가급적 뷰는 xml에서 작업하기
@@ -18,21 +19,47 @@ class MainActivity : AppCompatActivity() { //AppCompoatActivity 부모 클래스
         // 그래서, 코드에서 리소스의 갑을 불러올 때 항상, R.layout 등으로 시작해서 불러옵니다.
         // 화면을 출력하겠다. 레이아웃 7장 더 설명.
         // 뷰그룹 : 뷰(텍스트, 이미지, 인풋, 체크박스, 라디오..)를 모아주는 역할
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        //뷰 바인딩 기술을 통해서, 뷰들을 특정 바인딩이라는 객체에 모두 모아서,
+        //접근을 쉽게 해주는 기술.
+        //사용하기전에 항상, build.gradle 파일에 설정 후, sync now 적용해서 사용해야함.
+
+        //이 문법은 자동으로 생성된 바인딩 파일을 inflate 함수와, 인자는 : laoutInflater는 고정
+        //setContentView -> 화면에 그리기
+        //binding 변수에, 모두 뷰가 다 들어가 있음.
+        //결론, 여기서 필요한 뷰를 꺼내 사용한다.
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val button1 = findViewById<Button>(R.id.btn1)
         val img1 = findViewById<ImageView>(R.id.img1)
         var status = 0
 
-        //button1 눌러서 -> img show/hide
+        //button1을 뷰바인딩 기법으로 접근
         button1.setOnClickListener {
             if (status == 0) {
-                img1.visibility = View.INVISIBLE
+                binding.img1.visibility = View.INVISIBLE
                 status = 1
             } else {
-                img1.visibility = View.VISIBLE
+                binding.img1.visibility = View.VISIBLE
                 status = 0
             }
         }
+
+//        val button1 = findViewById<Button>(R.id.btn1)
+//        val img1 = findViewById<ImageView>(R.id.img1)
+//        var status = 0
+
+        //button1 눌러서 -> img show/hide
+//        button1.setOnClickListener {
+//            if (status == 0) {
+//                img1.visibility = View.INVISIBLE
+//                status = 1
+//            } else {
+//                img1.visibility = View.VISIBLE
+//                status = 0
+//            }
+//        }
     }
 }

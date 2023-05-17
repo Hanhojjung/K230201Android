2401f4353efab9d4e63799bdb04839d3b233837a
hanhojjung <ghwjd9269@naver.com>
2023-05-17 10:50:52 +0900

    2023 05 17 2교시

--- test12/src/main/java/com/example/test12/MainActivity.kt
+++ test12/src/main/java/com/example/test12/MainActivity.kt
@@ -1,8 +1,19 @@
 package com.example.test12
 
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.test12.databinding.ActivityMainBinding
+import com.example.test12.databinding.ItemRecyclerviewBinding
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,8 +35,63 @@ class MainActivity : AppCompatActivity() {
         //뷰홀더, 어댑터, 레이아웃 매니저, 적용.
         //클래스 2개 만들고,
 
+        //리사이클러뷰 적용하기
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerview.layoutManager = layoutManager
+        val adapter = MyAdapter(datas)
+        binding.recyclerview.adapter = adapter
+        binding.recyclerview.addItemDecoration(MyDecoration(this))
+
         //앱 바라는 뷰에, 툴바, 이미지 뷰, 리사이클러 뷰도
         //참고코드 : MainActivity378
 
+
+    }
+
+    //툴바 구성 시 옵션 추가 부분 필요
+    // 개발자가 직접 정의를 해야함.
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_378, menu)
+//        return super.onCreateOptionsMenu(menu)
+        return true
+    }
+    class MyViewHolder(val binding: ItemRecyclerviewBinding): RecyclerView.ViewHolder(binding.root)
+
+    class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+        override fun getItemCount(): Int{
+            return datas.size
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
+                = MyViewHolder(ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val binding=(holder as MyViewHolder).binding
+            binding.itemData.text= datas[position]
+        }
+    }
+
+    // 목록 아이템 3개 씩 나열하다가, 중간에 조금 더 간겨을 주고 하는 부분
+    class MyDecoration(val context: Context): RecyclerView.ItemDecoration() {
+
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            super.getItemOffsets(outRect, view, parent, state)
+            val index = parent.getChildAdapterPosition(view) + 1
+
+            if (index % 3 == 0) //left, top, right, bottom
+                outRect.set(10, 10, 10, 60)
+            else
+                outRect.set(10, 10, 10, 0)
+
+            view.setBackgroundColor(Color.parseColor("#28A0FF"))
+            ViewCompat.setElevation(view, 20.0f)
+
+        }
     }
 }

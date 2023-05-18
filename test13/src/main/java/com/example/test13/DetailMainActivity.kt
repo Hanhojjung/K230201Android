89ab539b83ee7f3ba2047478ce483df5314b92ea
hanhojjung <ghwjd9269@naver.com>
2023-05-18 10:43:53 +0900

    2023 05 18 2교시

--- test13/src/main/java/com/example/test13/DetailMainActivity.kt
+++ test13/src/main/java/com/example/test13/DetailMainActivity.kt
@@ -9,5 +9,24 @@ class DetailMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityDetailMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //인텐드에 등록된 데이터 가져오기
+        val data1 = intent.getStringExtra("data1")
+
+        //2번째 매개변수, 디폴트 값은 불러온 값이 없을 경우, 기본값이 대체됨
+        val data2 = intent.getIntExtra("data2",0)
+
+        binding.resultViewText.text = "data1키의 값 조회 : $data1, data2키의 값 조회 : $data2"
+
+        //후처리 작업 테스트
+        //버튼을 클릭시 후처리 작업.
+        binding.btn2.setOnClickListener {
+            //작업 set
+            intent.putExtra("result","후처리 데이터 값")
+            //결과 코드와, 인텐트 전달 작업
+            setResult(RESULT_OK,intent)
+            //현재 화면 종료
+            finish()
+        }
     }
 }
--- test13/src/main/java/com/example/test13/MainActivity.kt
+++ test13/src/main/java/com/example/test13/MainActivity.kt
@@ -1,21 +1,46 @@
 package com.example.test13
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.test13.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // onCreate 최초 한번 실행 시, binding 늦게 할당이 되어서, 마치 전역처럼 사용 가능.
+    // onCreate 함수 내부 뿐만 아니라, 다른 함수 내부에서도 사용가능.
+    lateinit var binding: ActivityMainBinding
+
+    // 생명주기 onCreate 최초 한번 실행
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //버튼 클릭 시, Detail Activity로 이동
         binding.btn1.setOnClickListener{
             //::class.java, 같은 앱의 액티비티 전달 시, 타입을 명시적 타입(명클)으로 한다.
             val intent = Intent(this@MainActivity,DetailMainActivity::class.java)
-            startActivity(intent)
+
+            //문자열, 정수를 설정해서, 보내는 데이터 설정/
+            intent.putExtra("data1","이상용")
+            intent.putExtra("data2",518)
+
+            //단순 화면 이동만 하는 함수
+//            startActivity(intent)
+            //후처리를 하는 함수를 사용해야 함.
+            startActivityForResult(intent,10)
+        }
+    }
+
+    // onCreate 밖에서, 후처리 방법 중 방법1, 가급적 사용 자제, 이유는 이후 버전이 나와서.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 10 && resultCode == Activity.RESULT_OK){
+            val result = data?.getStringExtra("result")
+            Log.d("lsy","result 값 조회 : $result")
+            binding.resultMainViewText.text = result
         }
     }
 }

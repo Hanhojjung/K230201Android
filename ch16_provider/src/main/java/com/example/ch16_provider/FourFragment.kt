54756412c5215182f95c84468f347fafc700b6f9
hanhojjung <ghwjd9269@naver.com>
2023-05-19 13:58:46 +0900

    2023 05 19 4교시

---
+++ ch16_provider/src/main/java/com/example/ch16_provider/FourFragment.kt
@@ -0,0 +1,22 @@
+package com.example.ch16_provider
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FourFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_four, container, false)
+    }
+
+
+
+}
